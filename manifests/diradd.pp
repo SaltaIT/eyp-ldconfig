3c88f2a2b162a4e3e92db1da7366ccb97bf30f03
Jordi Prats <jprats@systemadmin.es>
2016-10-24 16:29:47 +0200

    typo

--- manifests/diradd.pp
+++ manifests/diradd.pp
@@ -4,9 +4,9 @@ define ldconfig::diradd (
                           $order = '28',
                         ) {
 
-  if(! defined(Concat["/etc/ld.so.conf.d/${default}.conf"]))
+  if(! defined(Concat["/etc/ld.so.conf.d/${type}.conf"]))
   {
-    concat { "/etc/ld.so.conf.d/${default}.conf":
+    concat { "/etc/ld.so.conf.d/${type}.conf":
       ensure => 'present',
       owner  => 'root',
       group  => 'root',
@@ -14,15 +14,15 @@ define ldconfig::diradd (
       notify => Exec['eyp-ldconfig'],
     }
 
-    concat::fragment{ "/etc/ld.so.conf.d/${default}-puppet-managed.conf header":
-      target  => "/etc/ld.so.conf.d/${default}.conf",
+    concat::fragment{ "/etc/ld.so.conf.d/${type}.conf header":
+      target  => "/etc/ld.so.conf.d/${type}.conf",
       order   => '00',
       content => "# puppet managed file\n",
     }
   }
 
-  concat::fragment{ "/etc/ld.so.conf.d/${default}-puppet-managed.conf ${path}":
-    target  => "/etc/ld.so.conf.d/${default}.conf",
+  concat::fragment{ "/etc/ld.so.conf.d/${type}.conf ${path}":
+    target  => "/etc/ld.so.conf.d/${type}.conf",
     order   => $order,
     content => "${path}\n",
   }
