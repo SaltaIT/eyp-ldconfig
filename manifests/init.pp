a957f2ed947925965e502102f858b08fcedf4d93
Jordi Prats <jordi.prats@gmail.com>
2018-05-02 13:17:47 +0200

    lint

--- manifests/init.pp
+++ manifests/init.pp
@@ -1,15 +1,11 @@
-# == Class: ldconfig
-#
-# === ldconfig documentation
-#
 class ldconfig(
-                            $manage_package        = true,
-                            $package_ensure        = 'installed',
-                            $manage_service        = true,
-                            $manage_docker_service = true,
-                            $service_ensure        = 'running',
-                            $service_enable        = true,
-                          ) inherits ldconfig::params{
+                $manage_package        = true,
+                $package_ensure        = 'installed',
+                $manage_service        = true,
+                $manage_docker_service = true,
+                $service_ensure        = 'running',
+                $service_enable        = true,
+              ) inherits ldconfig::params{
   Exec {
     path => '/usr/sbin:/usr/bin:/sbin:/bin',
   }
